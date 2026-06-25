7855effa125aceb85f5084f16f624f4eca2b3a96
T123Minecraft <t123minecraft@qq.com>
2026-06-25 21:01:50 +0800

    Initial commit

---
+++ src/main/java/com/t123minecraft/t123s_test_mod/T123sTestModClient.java
@@ -0,0 +1,10 @@
+package com.t123minecraft.t123s_test_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class T123sTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/t123minecraft/t123s_test_mod/T123sTestModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.t123minecraft.t123s_test_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class T123sTestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
